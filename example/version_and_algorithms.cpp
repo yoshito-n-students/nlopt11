e31a8e1eef00002b88e98701b28e9cf804212c52
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2020-11-21 09:19:18 +0900

    add an example to print version & algorithms

---
+++ example/version_and_algorithms.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+#include <nlopt11.hpp>
+
+int main(int argc, char *argv[]) {
+
+  std::printf("nlopt v%d.%d.%d\n", nlopt::version_major(), nlopt::version_minor(),
+              nlopt::version_bugfix());
+  for (std::size_t i = 0; i < nlopt::NUM_ALGORITHMS; ++i) {
+    std::printf("%4zd: %s\n", i,nlopt::algorithm_name(static_cast<nlopt::algorithm>(i)));
+  }
+
+  return 0;
+}
