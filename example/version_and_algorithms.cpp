e1cc1ce62e9d89491d325db155fe7f9a9fec0b5b
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2020-11-24 09:34:05 +0900

    apply clang-format

--- example/version_and_algorithms.cpp
+++ example/version_and_algorithms.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
   std::printf("nlopt v%d.%d.%d\n", nlopt::version_major(), nlopt::version_minor(),
               nlopt::version_bugfix());
   for (std::size_t i = 0; i < nlopt::NUM_ALGORITHMS; ++i) {
-    std::printf("%4zd: %s\n", i,nlopt::algorithm_name(static_cast<nlopt::algorithm>(i)));
+    std::printf("%4zd: %s\n", i, nlopt::algorithm_name(static_cast<nlopt::algorithm>(i)));
   }
 
   return 0;
