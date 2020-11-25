0bb573ab333a573119dc19a0e855467bd25a451b
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2020-11-25 20:17:47 +0900

    add an example from the Umetani's book

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -2,6 +2,10 @@
 Changelog for package nlopt11
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
+0.0.2 (2020-XX-XX)
+------------------
+* Add more examples (umetani_book_3_9{a,b}, matlab_qp400)
+
 0.0.1 (2020-11-24)
 ------------------
 * Initial version tested on Ubuntu 18.04 and 20.04 with examples from nlopt, matlab and octave tutorials
---
+++ example/umetani_book_3_9a.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <vector>
+
+#include <nlopt11.hpp>
+
+int main(int argc, char *argv[]) {
+  // Form the problem (expecting a gradient-based algorithm).
+  // It is also found in a book "Shikkari Manabu Suuri Saitekika" by Umetani
+  nlopt::opt11 opt(/* algorithm = */ nlopt::LD_SLSQP, /* n_params = */ 2);
+  opt.set_min_objective([](const std::vector<double> &x, std::vector<double> &grad) -> double {
+    // gradients
+    if (!grad.empty()) {
+      grad[0] = 2. * x[0];
+      grad[1] = 2. * x[1];
+    }
+    // objective func
+    return x[0] * x[0] - x[1] * x[1];
+  });
+  opt.add_equality_constraint(
+      [](const std::vector<double> &x, std::vector<double> &grad) -> double {
+        // gradients
+        if (!grad.empty()) {
+          // grad[i] = d(constraint)/d(x[i])
+          grad[0] = 2. * x[0];
+          grad[1] = 8. * x[1];
+        }
+        // constraint
+        return x[0] * x[0] + 4. * x[1] * x[1] - 1;
+      },
+      /* tolerance = */ 1e-8);
+  opt.set_xtol_rel(1e-4);
+
+  // Solve it!
+  try {
+    std::vector<double> x(2, std::sqrt(0.2));
+    double minf;
+    nlopt::result result = opt.optimize(x, minf);
+    std::printf("found minimum at f(%f, %f) = %f\n", x[0], x[1], minf);
+  } catch (std::exception &e) {
+    std::printf("nlopt failed: %s\n", e.what());
+  }
+
+  return 0;
+}
